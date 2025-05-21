5f9f1e02784aef5bc71946564b30318cfea91da9
utinder <henrydangerfanboy123@gmail.com>
2025-05-20 21:59:23 -0700

    Update tralalerohub.lua

--- tralalerohub.lua
+++ tralalerohub.lua
@@ -1,6 +1,3 @@
-print("balls bals balls balls balls balls balls!!!!!!!!!!!!!")
-
-
 if game.PlaceId == 85896571713843 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/redsoul12q/tralalerohub/refs/heads/main/bubblegumsim.lua"))()
 end
