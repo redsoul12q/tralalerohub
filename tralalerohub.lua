e86fb452650c4d291b6ff9a3518d8e014d0460d7
utinder <henrydangerfanboy123@gmail.com>
2025-05-20 22:03:41 -0700

    Update tralalerohub.lua

--- tralalerohub.lua
+++ tralalerohub.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 85896571713843 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/redsoul12q/tralalerohub/refs/heads/main/bubblegumsim.lua"))()
+else
+game.Players.LocalPlayer:Kick("game not supported by tralaleroHub")
 end
