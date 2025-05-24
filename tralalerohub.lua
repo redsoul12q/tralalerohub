a25a73bbd707ac6b2872f71743aa5dac117eff6d
utinder <henrydangerfanboy123@gmail.com>
2025-05-23 20:40:49 -0700

    Update tralalerohub.lua

--- tralalerohub.lua
+++ tralalerohub.lua
@@ -1,3 +1,8 @@
+local Username = game.Players.LocalPlayer.Name
+
+print("Succesfully loaded tralaleroHub. Welcome, " .. Username .. "!")
+
+
 if game.PlaceId == 85896571713843 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/redsoul12q/tralalerohub/refs/heads/main/bubblegumsim.lua"))()
 else
